1dbd53bdb91d75cbbcf309128f827bf9cdf8dd75
Mark Tyers <marktyers@gmail.com>
2016-09-05 18:22:31 +0100

    fixed shell provisioner

--- 04 Build and Deployment/vagrant/src/server.js
+++ 04 Build and Deployment/vagrant/src/server.js
@@ -1,11 +1,13 @@
 
 'use strict'
-
+console.log('starting')
 const http = require('http')
 const defaultPort = 8080
 const statusOK = 200
 const server = http.createServer(function(req, res) {
+	console.log('incoming request')
 	res.writeHead(statusOK)
 	res.end('Goodbye World!')
 })
+console.log(`PORT environment var: ${process.env.PORT}`)
 server.listen(process.env.PORT || defaultPort)
